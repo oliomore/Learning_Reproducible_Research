be6bd7f6d9c3c8cadc9f3374eb262eaf181bde7c
unknown <you@example.com>
2021-03-31 13:18:46 -0300

    Desarrollo y practica Clase de Investigacion reproducible y RMd

--- Clase_2_Investigacion_Reproducible.R
+++ Clase_2_Investigacion_Reproducible.R
@@ -2,3 +2,17 @@
 library(tidyverse)
 library(knitr)
 library(kableExtra)
+
+plants <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv')
+actions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv')
+threats <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv')
+
+data("msleep")
+View(msleep)
+Tabla <- msleep %>% 
+  group_by(vore) %>% 
+  summarise_at("sleep_total",.funs = list(Mean = mean, SD = sd)) %>% 
+  dplyr::filter(!is.na(vore)) %>% 
+  arrange(desc(Mean))
+
+kable(Tabla)
